9ea10a52243315c56c6faa11947cb0294b2ed74c
Error <pashasob2009@gmail.com>
2025-01-06 22:35:29 +0300

    Update 2
    Add sensitivity

--- ArduinoMouseControl.py
+++ ArduinoMouseControl.py
@@ -4,10 +4,16 @@ import pyautogui
 ser = serial.Serial('COM3', 9600)
 pyautogui.FAILSAFE = False
 
+sensitivity = 2.0
+
 while True:
     data = ser.readline().decode('utf-8').strip()
     if data.startswith("X:"):
         parts = data.split(" ")
         x_move = int(parts[0].split(":")[1])
         y_move = int(parts[1].split(":")[1])
+
+        x_move *= sensitivity
+        y_move *= sensitivity
+
         pyautogui.move(x_move, y_move)
